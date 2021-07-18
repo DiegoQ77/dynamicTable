8e2ab1f410e8d99af6a252233e9d15f317a4f0c1
Diego <diego.quisbert77@gmail.com>
2021-07-18 01:27:43 -0500

    fuction to add new row wit js

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,10 +1,17 @@
 const { event } = require('jquery')
 
+const bodyTable = document.querySelector('.table tbody')
+// console.log(barCodeInput)
+
 document.addEventListener('DOMContentLoaded', () => {
   const barCodeInput = document.querySelector('.table tbody tr input')
   const nameProduct = document.querySelector('#name')
   const countProducts = document.querySelector('#amount')
   const total = document.querySelector('#total')
+
+  const barcodeROw = document.querySelector('.table tbody tr')
+
+  // variables
 
   // console.log(nameProduct)
   // console.log(barCodeInput)
@@ -14,13 +21,17 @@ document.addEventListener('DOMContentLoaded', () => {
   barCodeInput.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
       e.preventDefault()
-      console.log(document.getElementById('row-table').value)
-      console.log(e)
-      nameProduct.innerHTML = 'nuevo'
-      countProducts.innerHTML = 5
-      total.innerHTML = 5.50
+      const row = document.createElement('tr')
+      row.innerHTML = agregarFila()
+      // console.log(document.getElementById('row-table').value)
+      // console.log(e)
+      // nameProduct.innerHTML = 'nuevo'
+      // countProducts.innerHTML = 5
+      // total.innerHTML = 5.50
 
-      $('table').find('tbody').append(agregarFila())
+      // $('table').find('tbody').prepend(agregarFila(e))
+
+      bodyTable.insertBefore(row, barcodeROw)
     }
   })
 })
@@ -32,13 +43,19 @@ const readBarCode = () => {
 }
 
 function agregarFila () {
+  console.log('element from agregar Fial')
+  // console.log(e)
   return (`
     <tr>
-      <td><input type="number" class="form-control" id="row-table" placeholder="Example input"></td>
+      <td>123123123</td>
       <td>Coca Cola</td>
       <td>cantidad</td>
       <td>6.50</td>
       <td><button type='button' class='btn btn-outline-danger'>Eliminar</td>
     </tr>
   `)
+}
+
+function getValues () {
+
 }
